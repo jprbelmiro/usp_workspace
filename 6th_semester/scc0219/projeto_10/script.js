cf54a3bfe212c5d6014030139b584a65996d23b0
Micanga <matheus.aparecido.alves@usp.br>
2019-05-09 16:24:33 -0300

    uploading full project 10

--- 6th_semester/scc0219/projeto_10/script.js
+++ 6th_semester/scc0219/projeto_10/script.js
@@ -1,167 +1,92 @@
-var periodoTextBox = document.getElementById("input-days");
-var vendasCheckBox = document.getElementById("sells");
-var comprasCheckBox = document.getElementById("buy");
-var descCheckBox = document.getElementById("gt");
-var ascCheckBox = document.getElementById("lt");
+var select = document.getElementById("buyitens");
+var botao_incluir = document.getElementById("include");
+var botao_excluir = document.getElementById("remove-buy");
+var data_input = document.getElementById("date");
 
-var botao = document.querySelector("Button");
-var body = document.querySelector("body");
-var div_table = null;
+//botao_incluir.disabled = true;
 
 //criando objeto cotacao
-var Cotacao = function(data, cotacao) {
+var Cotacao = function(data, cotacaoVenda, cotacaoCompra) {
   this.data = data;
-  this.cotacao = cotacao;
+  this.cotacaoVenda = cotacaoVenda;
+  this.cotacaoCompra = cotacaoCompra;
 }
 
-var Cotacoes = function(){
-	this.cotacoes = [];
-}
 
-//metodo que le cotacoes de acordo com o  
-//tipo que o usuario solicitou e adciona na lista
-Cotacoes.prototype.addCotacoes = function(cotacao){
-	var novaCotacao;
-	for(i = 0; i < cotacao.length; i++){
-		if(comprasCheckBox.checked){
-			novaCotacao = new Cotacao(cotacao[i].dataHoraCotacao, cotacao[i].cotacaoCompra); 
-		}
-		else if(vendasCheckBox.checked){
-			novaCotacao = new Cotacao(cotacao[i].dataHoraCotacao, cotacao[i].cotacaoVenda);
-		}
-		if(novaCotacao != undefined) {
-			this.cotacoes.push(novaCotacao);
-		}
-	}
-}
+Cotacao.prototype.appendCotacao = function(){
 
-//Metodo que ordena as cotacoes de acordo com
-// a ordem que o usuario solicitou
-Cotacoes.prototype.ordenarCotacoes = function (){
-	if(ascCheckBox.checked){
-		this.cotacoes.sort(function(a, b){return a.cotacao-b.cotacao})
-	}else if(descCheckBox.checked){
-		this.cotacoes.sort(function(a, b){return b.cotacao-a.cotacao})
-	}
-}
-
-//metodo que insere as cotacoes na pagina
-Cotacoes.prototype.appendCotacoes = function(){
-	//caso existir uma retira-lá para nao acumular tabelas
-	if(div_table != null) div_table.parentNode.removeChild(div_table);
-	//Adicionando nova Div para inserir dados
-	div_table = document.createElement("div");
-	body.appendChild(div_table);
-	//Criando center para inserir Titulo da tabela
-	var center = document.createElement("center");
-	center.textContent = "COTAÇÕES SELECIONADAS PARA VISUALIZAÇÃO";
-	div_table.appendChild(center);
-	//Caixa da div
-	var div_box = document.createElement("div");
-	div_box.setAttribute('class', 'myBox');
-	div_table.appendChild(div_box);
-	//Como ela se dispoem
-	var div_row = document.createElement("div");
-	div_row.setAttribute('class', 'row');
-	div_box.appendChild(div_row);
-	//coluna de datas
-	var div_col_data = document.createElement("div");
-	div_col_data.setAttribute('class', 'column-list');
-	div_row.appendChild(div_col_data);
-	//nomeando e setando uma lista cotacoes
-	var label_data = document.createElement("label");
-	label_data.setAttribute("for", "data-list");
-	label_data.innerHTML = "<b>Datas</b>";
-	div_col_data.appendChild(label_data);
-	var ul_data = document.createElement("ul");
-	ul_data.setAttribute('id', 'data-list');
-	ul_data.setAttribute('style', 'list-style-type:none;');
-	div_col_data.appendChild(ul_data);
-	//coluna de cotacoes
-	var div_col_cotacao = document.createElement("div");
-	div_col_cotacao.setAttribute('class', 'column-list');
-	div_row.appendChild(div_col_cotacao);
-	//nomeando e setando uma lista para cotacoes
-	var label_cot = document.createElement("label");
-	label_cot.setAttribute("for", "cot-list");
-	label_cot.innerHTML = "<b>Cotações</b>";
-	div_col_cotacao.appendChild(label_cot);
-	var ul_cotacao = document.createElement("ul");
-	ul_cotacao.setAttribute('id', 'cot-list');
-	ul_cotacao.setAttribute('style', 'list-style-type:none;');
-	div_col_cotacao.appendChild(ul_cotacao);
-	
-	var li_data;
-	var li_cot;	
 	//populando a lista de data e cotacao com os dados
-    for (var i = 0; i < this.cotacoes.length; i++) {
-        li_data = document.createElement("li");
-        li_cot = document.createElement("li");
-        li_data.textContent = this.cotacoes[i].data.toString();
-		li_cot.textContent += this.cotacoes[i].cotacao;
-        ul_data.appendChild(li_data);
-        ul_cotacao.appendChild(li_cot);
-    }
+    option = document.createElement("option");
+
+    dataSpan = document.createElement("span");
+    dataSpan.textContent = this.data.getDate() + "/" + (this.data.getMonth()+1) + 
+    		"/" + this.data.getFullYear() +"; ";
+    option.appendChild(dataSpan);
+
+    cotacaoCompraSpan = document.createElement("span");
+    cotacaoCompraSpan.textContent = "R$ " + this.cotacaoCompra + "; ";
+    option.appendChild(cotacaoCompraSpan);
+
+    cotacaoVendaSpan = document.createElement("span");
+    cotacaoVendaSpan.textContent = "R$ " + this.cotacaoVenda;
+    option.appendChild(cotacaoVendaSpan);
+
+    /*text = this.data.getDate() + "/" + (this.data.getMonth()+1) + 
+    		"/" + this.data.getFullYear() +  
+    		";" + this.cotacaoVenda + ";" + this.cotacaoCompra;
+    option.textContent = text;*/
+    select.appendChild(option);
 }
 
 function fetchCotacoes(){
-	var cotacoes = new Cotacoes();
 	var url;
 	var cotacao;
-
-	//ler o periodo que o usuario pediu e setar datas do dia de hoje até
-	//o dia de hoje menos o numero de dias que o usuario inseriu
-	var dias = Number(periodoTextBox.value);
-	var dataInicial = new Date()
-	dataInicial.setDate(dataInicial.getDate() - dias);
-	var dataFinal = new Date();
-	
-	//verificando se os text está correto
-	if(dias < 10 || dias > 90){
-		alert("fornecer um período de 10 (min) a 90 (max) dias para o qual solicita cotação.");
+	//verifytextbox
+	var data = new Date(data_input.value.replace(/-/g, '\/'));
+	if(isNaN(data.getTime())){
+		alert("Data Invalida\n");
 		return;
 	}
-	if(periodoTextBox.checkValidity() == false){
-		alert("Insira o numero de dias.")
-		return;
-	}
-
-	//criando URL para consultar o JSOn
-	url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''
-			+ (dataInicial.getMonth()+1) + '-' + dataInicial.getDate() + '-' + dataInicial.getFullYear() 
-			+'\'&@dataFinalCotacao=\''
-			+ (dataFinal.getMonth()+1) + '-' + dataFinal.getDate() + '-' + dataFinal.getFullYear()
-			+ '\'&$top=100&$format=json';
-
+	url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=\''
+			+ (data.getMonth()+1) + '-' + data.getDate() + '-' + data.getFullYear() 
+			+'\'&$top=100&$format=json';
+	console.log(url);
 	fetch(url).then(function(response) {
 		return response.json();
 	}).then(function(json) {
 		//atribuindo a cotacao o valor recuperado do fetch
-		cotacao = json.value;
-		//
-		cotacoes.addCotacoes(cotacao);
-		cotacoes.ordenarCotacoes();
-		cotacoes.appendCotacoes();
+		dados = json.value[0];
+		cotacao = new Cotacao(data, Number(dados.cotacaoVenda), Number(dados.cotacaoCompra));
+		
+		var existe = false;
+		for (var i = select.options.length - 1; i >= 0; i--) {
+			if(select.options[i].text.split(';',1)[0] == cotacao.data.getDate() + "/" + (cotacao.data.getMonth()+1) + "/" +
+			 cotacao.data.getFullYear()){
+				alert("Essa data já foi inserida em sua tabela.");
+				existe = true
+			}
+		}
+		if(!existe){
+			cotacao.appendCotacao();
+		}
 	}).catch(function(err) {
+		alert("Não foi possível encontrar a cotação referete a esta data no banco de dados.");
 		console.log('Fetch problem: ' + err.message);
 	});
-		
 	
-	return cotacoes;
 }
 
-//Procurar Cotacoes quando clicar no botao
-botao.addEventListener("click", fetchCotacoes);
+function removeCotacoes(){
+	//vamos deletar da drop list enquanto nao ouver algo selecionado
+	while(select.selectedIndex != -1){
+		select.remove(select.selectedIndex);
+	}
+}
 
-//nao deixar checkar dois eventos checkados ao mesmo tempo
-descCheckBox.addEventListener("click", function(){ 
-	ascCheckBox.checked = false;});
+data_input.onchange = function(){
+   botao_incluir.disabled = false;
+}
 
-ascCheckBox.addEventListener("click", function(){ 
-	descCheckBox.checked = false;});
+botao_incluir.addEventListener("click", fetchCotacoes);
 
-vendasCheckBox.addEventListener("click", function(){ 
-	comprasCheckBox.checked = false;});
-
-comprasCheckBox.addEventListener("click", function(){ 
-	vendasCheckBox.checked = false;});
+botao_excluir.addEventListener("click", removeCotacoes);
